d8eec3e6b2d76deb5091bffd9d8a1adc7812af2e
Awies <amulla@ucsd.edu>
2023-01-28 18:08:49 -0800

    Generated plots for VICON Data

--- code/load_data.py
+++ code/load_data.py
@@ -17,9 +17,9 @@ def read_data(fname):
   return d
 
 dataset="1"
-cfile = "./../data/cam/cam" + dataset + ".p"
-ifile = "./../data/imu/imuRaw" + dataset + ".p"
-vfile = "./../data/vicon/viconRot" + dataset + ".p"
+cfile = "./../../data/cam/cam" + dataset + ".p"
+ifile = "./../../data/imu/imuRaw" + dataset + ".p"
+vfile = "./../../data/vicon/viconRot" + dataset + ".p"
 
 ts = tic()
 camd = read_data(cfile)
@@ -28,12 +28,24 @@ vicd = read_data(vfile)
 toc(ts,"Data import")
 
 #============================================================================#
+#My Code
+#============================================================================#
 
 import matplotlib.pyplot as plt
 import numpy as np
+import transforms3d
 
 vicon = vicd['rots']
 
+orientation = []
 
+for i in range(len(vicon[0][0])):
+  rotation_matrix = [[vicon[0][0][i], vicon[0][1][i], vicon[0][2][i]],[vicon[1][0][i], vicon[1][1][i], vicon[1][2][i]], [vicon[2][0][i], vicon[2][1][i], vicon[2][2][i]]]
+  R = np.asmatrix(rotation_matrix)
+  quaternions = transforms3d.quaternions.mat2quat(R)
+  euler = transforms3d.euler.quat2euler(quaternions)
+  orientation.append(list(euler))
 
-
+orientation = np.array(orientation)
+plt.plot(orientation[:,0])
+plt.show()
